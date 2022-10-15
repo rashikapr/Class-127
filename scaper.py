9164c884f7ce59f7f9c2a64e159925c0bc757a47
rashikapr <74289013+rashikapr@users.noreply.github.com>
2022-10-15 19:02:58 +0530

    Add files via upload

---
+++ scaper.py
@@ -0,0 +1,30 @@
+import csv
+import time
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+START_URL = "https://exoplanets.nasa.gov/exoplanet-catalog/"
+browser = webdriver.Chrome(r"C:\Users\hp\Desktop\Python-class\Class-127\chromedriver.exe")
+browser.get(START_URL)
+time.sleep(10)
+
+def scrape():
+    headers = ["name", "light_years_from_earth", "planet_mass", "stellar_magnitude", "discovery_date"]
+    planet_data = []
+    
+        soup = BeautifulSoup(browser.page_source, "html.parser")
+        for ul_tag in soup.find_all("ul", attrs={"class", "exoplanet"}):
+            li_tags = ul_tag.find_all("li")
+            temp_list = []
+            for index, li_tag in enumerate(li_tags):
+                if index == 0:
+                    temp_list.append(li_tag.find_all("a")[0].contents[0])
+                else:
+                    try:
+                        temp_list.append(li_tag.contents[0])
+                    except:
+                        temp_list.append("")
+            planet_data.append(temp_list)
+        
+scrape()
